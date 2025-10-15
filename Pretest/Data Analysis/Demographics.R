2e7d9ef059a050d78f7a2df7a9bf062eea9766ab
HedwigOl <92802991+HedwigOl@users.noreply.github.com>
2025-10-15 16:04:30 +0200

    Create Demographics.R

---
+++ Pretest/Data Analysis/Demographics.R
@@ -0,0 +1,50 @@
+# Read cleaned data file
+df <- read.csv("data/cleaned_data_pretest.csv")
+
+# Create table of Straattaal use distribution
+table(df$straattaal_use)
+
+# Create seperate dfs for different Straattaal use groups
+answers <- unique(df$straattaal_use)
+straattaal_dfs <- lapply(answers, function(a) {subset(df, straattaal_use == a)})
+names(straattaal_dfs) <- answers
+
+# Gender distribution
+table(df$gender)
+gender_strttlgroups <- lapply(straattaal_dfs, function(df) {table(df$gender)})
+gender_strttlgroups
+
+# Age distribution
+min_age <- min(df$age, na.rm = TRUE)
+min_age
+max_age <- max(df$age, na.rm = TRUE)
+max_age
+mean_age <- mean(df$age, na.rm = TRUE)
+mean_age_strttlgroups <- sapply(straattaal_dfs, function(df) {mean(df$age, na.rm = TRUE)})
+mean_age
+mean_age_strttlgroups
+
+sd_age <- sd(df$age, na.rm = TRUE)
+sd_age_strttlgroups <- sapply(straattaal_dfs, function(df) {sd(df$age, na.rm = TRUE)})
+sd_age
+sd_age_strttlgroups
+
+# Randstad distribution
+table(df$randstad)
+randstad_strttlgroups <- lapply(straattaal_dfs, function(df) {table(df$randstad)})
+randstad_strttlgroups
+
+# Education distribution
+table(df$education)
+education_strttlgroups <- lapply(straattaal_dfs, function(df) {table(df$education)})
+education_strttlgroups
+
+# Birth country distribution
+table(df$born_nl)
+birth_country_strttlgroups <- lapply(straattaal_dfs, function(df) {table(df$born_nl)})
+birth_country_strttlgroups
+
+# Birth country parents distribution
+table(df$parents_born_nl)
+birth_countryparents_strttlgroups <- lapply(straattaal_dfs, function(df) {table(df$parents_born_nl)})
+birth_countryparents_strttlgroups
