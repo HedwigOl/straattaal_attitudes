c74c1b1ef966fd6a9580a66163f2c2ddc798ce5a
HedwigOl <92802991+HedwigOl@users.noreply.github.com>
2025-12-10 16:04:15 +0100

    Added comments

--- IAT/main.js
+++ IAT/main.js
@@ -1,5 +1,4 @@
 // --- MAIN ---
-// File creates and starts jspsych timeline
 
 // Save data to data server at the end of the experiment
 let jsPsych = initJsPsych({
@@ -30,19 +29,7 @@ var environmentCheck = {
   type: jsPsychHtmlButtonResponse,
   stimulus: style_UU + environmentText,
   choices: ['Verder'],
-  button_html: '<button class="jspsych-btn" disabled>%choice%</button>', // only allow continueing when all boxess are checked
-  data: {stimulus: "environment_check"},
-  on_load: function() {
-    const checkboxes = document.querySelectorAll('.check-item');
-    const button = document.querySelector('.jspsych-btn');
-
-    checkboxes.forEach(box => {
-      box.addEventListener('change', () => {
-        const allChecked = Array.from(checkboxes).every(cb => cb.checked);
-        button.disabled = !allChecked;
-      });
-    });
-  }
+  data: {stimulus: "environment_check"}
 };
 
 // Discussion of the general IAT including the different stimuli and their categorisation
@@ -55,19 +42,19 @@ let generalIatInstruction = {
 
 // Create full timeline of all IAT blocks
 function createFullIAT (cb_keys){
-  let block1 = createIATBlock(cb_keys.keyConfigurationBlock1, targetStimuli, true, 1, 1)
-  let block2 = createIATBlock(cb_keys.keyConfigurationBlock2, attributeStimuli_b2, true, 2, 2)
-  let block3 = createIATBlock(cb_keys.keyConfigurationBlock3, alternateStimuli(targetStimuli_A, targetStimuli_B, attributeStimuli_A, attributeStimuli_B, cb_keys.keyConfigurationBlock3), true, 3, 3)
-  let block4 = createIATBlock(cb_keys.keyConfigurationBlock3, alternateStimuli(targetStimuli_A, targetStimuli_B, attributeStimuli_A, attributeStimuli_B, cb_keys.keyConfigurationBlock3), false, 3, 4)
-  let block5 = createIATBlock(cb_keys.keyConfigurationBlock4, attributeStimuli_b5, true, 4, 5)
-  let block6 = createIATBlock(cb_keys.keyConfigurationBlock5, alternateStimuli(targetStimuli_A, targetStimuli_B, attributeStimuli_A, attributeStimuli_B, cb_keys.keyConfigurationBlock5), true, 5, 6)
-  let block7 = createIATBlock(cb_keys.keyConfigurationBlock5, alternateStimuli(targetStimuli_A, targetStimuli_B, attributeStimuli_A, attributeStimuli_B, cb_keys.keyConfigurationBlock5), false, 5, 7)
+  let block1 = createIATBlock(cb_keys.keyConfigurationBlock1, targetStimuli, 1)
+  let block2 = createIATBlock(cb_keys.keyConfigurationBlock2, attributeStimuli_b2, 2)
+  let block3 = createIATBlock(cb_keys.keyConfigurationBlock3, alternateStimuli(targetStimuli_A, 
+    targetStimuli_B, attributeStimuli_A, attributeStimuli_B, cb_keys.keyConfigurationBlock3), 3)
+  let block4 = createIATBlock(cb_keys.keyConfigurationBlock4, attributeStimuli_b5, 4)
+  let block5 = createIATBlock(cb_keys.keyConfigurationBlock5, alternateStimuli(targetStimuli_A, 
+    targetStimuli_B, attributeStimuli_A, attributeStimuli_B, cb_keys.keyConfigurationBlock5), 5)
 
-  const blocks = [block1, block2, block3, block4, block5, block6, block7].map(b => b || []);
+  const blocks = [block1, block2, block3, block4, block5].map(b => b || []);
   return { timeline: blocks.flat() };
 }
 
-//Question about being disturbed in the experiment
+// Question about being disturbed in the experiment
 let disturbedQuestion = {
   type: jsPsychSurveyText,
   preamble: style_UU + "",
@@ -82,11 +69,12 @@ let disturbedQuestion = {
   ]
 };
 
+// Demographics questionnaire
 let demographics = {
   timeline : [demographics1, demographics2]
 };
 
-//End screen and redirection to Prolific (TODO: redirect to prolific site)
+// End screen and redirection to Prolific (TODO: redirect to prolific site)
 let end = {
   type: jsPsychHtmlButtonResponse,
   stimulus: style_UU + endExperiment,
@@ -106,9 +94,10 @@ function main() {
     const cb_keys = setupKeyConfiguration(group_name);
     let IAT = createFullIAT(cb_keys);
 
+    // Add participant ID
     jsPsych.data.addProperties({subject_ID:  Math.floor(Math.random() * 10000) + 1,
                                 group: group_name});
-
+                                          
     let timeline = [consentProcedure, prolificID, environmentCheck, generalIatInstruction, 
                     IAT, explicitQuestionnaire, disturbedQuestion, demographics, end];
 
