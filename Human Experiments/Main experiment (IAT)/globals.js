8733ab329f6f8efb8847c0260fb946b04812c87e
HedwigOl <92802991+HedwigOl@users.noreply.github.com>
2026-04-28 15:48:09 +0200

    Create globals.js

---
+++ Human Experiments/Main experiment (IAT)/globals.js
@@ -0,0 +1,19 @@
+// --- GLOBALS ---
+
+// Access key used to upload data to the server.
+const ACCESS_KEY = '';
+
+// Amount of repetitions of each stimulus in each block
+const REP_TARGET = 4;
+const REP_ATTRIBUTE = 3;
+
+// Keys used as left and right button
+const LEFT_KEY  = 'f';
+const RIGHT_KEY = 'j';
+
+// Colors used for the target and attribute stimuli
+const COLOR_TARGET = 'black';
+const COLOR_ATTRIBUTE = 'blue';
+
+// Interval between trials
+const TRIAL_INTERVAL = 250;
